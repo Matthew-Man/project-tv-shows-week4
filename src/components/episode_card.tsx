538bc216cb6ccdb1900b8f33fcb973472c65d228
Matthew Man <matthew08m@gmail.com>
2021-02-19 11:41:55 +0000

    season code and summary regex added - title styling updated

--- src/components/episode_card.tsx
+++ src/components/episode_card.tsx
@@ -30,19 +30,26 @@ function EpisodeCard(props : IEpisode) {
     const {id, url, name, season, number, type, airdate, airtime, airstamp, runtime, image, summary, _links} = props;
     const imageAltDesc = name;
 
+    const filtedUnwantedTags = (unfiltered: string): string => unfiltered.replace(/(<([^>]+)>)/ig, "");
+    const createSeasNumber = season < 10 ? `0${season}` : season;
+    const createEpNumber = number < 10 ? `0${number}` : number;
+    
+
     return (
         <div className="episode-card">
             <div className="title-container">
-                <h2>{name} - S{season}E{number}</h2>
+                <span className="title">{name}</span><br/>
+                <span className="episode-code">S{createSeasNumber}E{createEpNumber}</span>
             </div>
             <div className="image-container">
                 <img src={image.medium} alt={imageAltDesc}/>
             </div>
             <div className="text-continer">
-                <p>{summary.substring(3).slice(0,-4)}</p>
+                <p>{filtedUnwantedTags(summary)}</p>
             </div>
         </div>
     );
 };
+
 
 export default createEpisodeCard;
