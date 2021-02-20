55cfe11855d27d89e9d5be9dbc01e429a930efc6
Matthew Man <matthew08m@gmail.com>
2021-02-20 01:24:04 +0000

    attempt at fetching data from API

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,53 @@ import './App.css';
 import { IEpisode } from "./components/interface";
 
 
+//Fetch API - test "https://api.tvmaze.com/shows/82/episodes" 
+// function fetchData() {
+//     return fetch("https://api.tvmaze.com/shows/82/episodes")
+//         .then(response => {
+//             // console.log(response);
+//             if (!response.ok) {
+//                 throw Error("Error")
+//             } 
+//             return response.json()
+//         })
+//         .then(data => {return data})
+//         .catch(error => console.log(error))
+// };
+
+// console.log(fetchData())
+
+// async function getDataset() {
+//     // let data: [];
+//     let response = await fetch("https://api.tvmaze.com/shows/82/episodes");
+//     // console.log(response.json()); // this line will "wait" for the previous to be completed
+//     return response.json().then(value => value);
+// }
+
+// getDataset()
+
 function App() {
+    // let emptyEpisodesArray: IEpisode[] = [];
     const episodesArray = data;
+    // const [episodesArray, setEpisodesArray] = useState(emptyEpisodesArray);
     const [searchTerm, setSearchTerm] = useState("");
     const [dropdownSelect, setDropdown] = useState("0");
-
-
+    
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value);
     const handleDropdownChange = (e: React.ChangeEvent<HTMLInputElement>) => {setDropdown(e.target.value); setSearchTerm("")};
-
+    
+    
+    async function getDataset() {
+        const url = "https://api.tvmaze.com/shows/82/episodes";
+        const response = await fetch(url);
+        const data = await response.json();
+        // setEpisodesArray(data) // constantly fetching and updating state
+        console.log(data)
+    }
+    
+    // getDataset()
+    // console.log(episodesArray)
+    // setInterval(getDataset, 60000 * 60)
 
     function isSearchResult(episode: IEpisode, term: string): boolean {
         const isNameIncluded = episode.name.toLowerCase().includes(term);
